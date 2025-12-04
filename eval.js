8fef9c5e396ceef5e2249d9b4176a831360f09b3
surajkanse88 <surajkanse88@gmail.com>
2025-12-04 12:17:27 +0530

    Document risks associated with eval usage
    Added a note about the risks of using eval.

--- eval.js
+++ eval.js
@@ -4,3 +4,9 @@ console.log(eval("2 + 2") === eval("4"));  // true
 console.log(eval("2 + 2") === eval(new String("2 + 2")));   // false
 
 // takes a string and runs it as JavaScript code.
+
+
+eval("delete everything"); // Risky code]
+
+
+
