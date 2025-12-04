9f49bf8db18f6e393b681ec45243d34d260c57ac
surajkanse88 <surajkanse88@gmail.com>
2025-12-04 16:36:40 +0530

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,8 @@
+JavaScript arrays are resizable.
+  
+Can contain a mix of different data types. (Heterogenous type of data)
+
+JavaScript array-copy operations create shallow copies
+
+keys are in the form of indices (0,1,.....)
+
