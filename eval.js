1e2cc208ff203f5c8da9ef3484a40519c2baaf24
surajkanse88 <surajkanse88@gmail.com>
2025-12-04 11:45:00 +0530

    Refactor eval.js to improve console output

---
+++ eval.js
@@ -0,0 +1,6 @@
+console.log(eval("2 + 2"));   // 4
+console.log(eval(new String("2 + 2")));  // 2 + 2
+console.log(eval("2 + 2") === eval("4"));  // true
+console.log(eval("2 + 2") === eval(new String("2 + 2")));   // false
+
+// takes a string and runs it as JavaScript code.
