80168ec2319741f8a68bf9afcfe5b396f2c0b86a
surajkanse88 <surajkanse88@gmail.com>
2025-12-04 12:20:26 +0530

    Implement test function for eval scope behavior
    Add test function demonstrating eval usage with local and global scope.

--- eval.js
+++ eval.js
@@ -9,4 +9,14 @@ console.log(eval("2 + 2") === eval(new String("2 + 2")));   // false
 eval("delete everything"); // Risky code]
 
 
+function test()
+{
+  const x = 2;
+  const y = 4;
+  // Direct call, uses local scope
+  console.log(eval("x + y")); // Result is 6
+  // Indirect call, uses global scope
+  console.log(eval?.("x + y")); // Throws because x is not defined in global scope
+}
+
 
